8b9d1e46698bee0ede68877f50e34e118b139645
yuvhrajVirSinghVerma <vermayuvhraj@gmail.com>
2024-10-19 17:11:42 +0530

    Added test for single number input

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -4,4 +4,8 @@ describe('Calculator', () => {
     it('should return 0 for an empty string', () => {
         assert.strictEqual(calculator(''), 0);
     });
+
+    it('should return the number for a single number', () => {
+        assert.strictEqual(calculator('1'), 1);
+    });
 })
