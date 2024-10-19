258bc2af7022083ae56ce4ba53da8aefcf25eb09
yuvhrajVirSinghVerma <vermayuvhraj@gmail.com>
2024-10-19 17:28:53 +0530

    Implemented logic for sum of two numbers

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-export function calculator(numbers){
-    if (!numbers) return 0;
-    return parseInt(numbers);
+export function calculator(numbers) {
+  if (!numbers) return 0;
+  let numArr = numbers.split(",");
+  let sum = 0;
+  numArr.map((num) => {
+    sum += parseInt(num);
+  });
+  return sum
 }
