939e5b05444e0bfdbe586366178a9806d058fdfc
yuvhrajVirSinghVerma <vermayuvhraj@gmail.com>
2024-10-19 17:41:17 +0530

    implemented logic to handle new lines in input

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 export function calculator(numbers) {
   if (!numbers) return 0;
-  let numArr = numbers.split(",");
+  let numArr = numbers.split(/[\n,]/);
+  console.log("numArr ",numArr)
   let sum = 0;
   numArr.map((num) => {
     sum += parseInt(num);
