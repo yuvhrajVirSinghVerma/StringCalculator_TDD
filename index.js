14f203026e3c67a13ed06487006ed58064410fcf
yuvhrajVirSinghVerma <vermayuvhraj@gmail.com>
2024-10-19 18:00:58 +0530

    Implemented logic for  custom delimiters

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 export function calculator(numbers) {
   if (!numbers) return 0;
-  let numArr = numbers.split(/[\n,]/);
-  console.log("numArr ",numArr)
+
+  let delimiter = ",";
+  if (numbers.startsWith("//")) {
+    let splits = numbers.split("\n");
+    delimiter = splits[0].substring(2);
+    numbers = splits[1];
+  }
+  let numArr = numbers.split(new RegExp(`[${delimiter}\n]`));
   let sum = 0;
   numArr.map((num) => {
     sum += parseInt(num);
   });
-  return sum
+  return sum;
 }
